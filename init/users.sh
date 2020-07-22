f15ec6126b4076173a2879e1400b12a0449614f1
Jonathan Seifert <jonathan.seifert@student.uni-halle.de>
2020-07-22 18:08:58 +0200

    users.sh veraendert

--- init/users.sh
+++ init/users.sh
@@ -4,6 +4,6 @@ set -e
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
     CREATE USER Administrator;
-    ALTER USER Administrator  WITH PASSWORD 'Admin123@';
+    ALTER USER Administrator  WITH PASSWORD '07c19';
 EOSQL
 
