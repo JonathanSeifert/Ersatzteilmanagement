2b5a98a01a009d784d9d3f5c4aa3a585ed28ceb5
Jonathan Seifert <jonathan.seifert@student.uni-halle.de>
2020-07-24 15:45:46 +0200

    docker-compose.yml und init/users.sh

--- init/users.sh
+++ init/users.sh
@@ -3,7 +3,7 @@
 set -e
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-    CREATE USER Administrator;
-    ALTER USER Administrator  WITH PASSWORD '07c19';
+    CREATE USER Admin;
+    ALTER USER Admin WITH PASSWORD '07c19';
 EOSQL
 
