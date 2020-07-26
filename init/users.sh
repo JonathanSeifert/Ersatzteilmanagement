4cc694f55aed69f7a92a98eb9dfc975274d3f4bb
ajtrs <jonathan.seifert@student.uni-halle.de>
2020-07-26 16:59:27 +0200

    lagerist und abteilungsleiter als Rollen hinzugefuegt

--- init/users.sh
+++ init/users.sh
@@ -2,8 +2,11 @@
 
 set -e
 
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-    CREATE USER Admin;
-    ALTER USER Admin WITH PASSWORD '07c19';
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER"  --dbname "$POSTGRES_DB" <<-EOSQL
+    CREATE USER admin;
+    ALTER USER admin WITH PASSWORD 'data';
+    CREATE USER lagerist;
+    ALTER ROLE lagerist WITH PASSWORD 'logistik';
+    CREATE USER abteilungsleiter;
+    ALTER ROLE abteilungsleiter WITH PASSWORD 'prozess';
 EOSQL
-
