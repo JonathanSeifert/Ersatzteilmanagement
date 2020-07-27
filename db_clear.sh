c8585730b725f673cbece897660af992f78a6e6c
ajtrs <jonathan.seifert@student.uni-halle.de>
2020-07-28 00:29:34 +0200

    Skripte zur Datenbanksteuerung

---
+++ db_clear.sh
@@ -0,0 +1,7 @@
+#/bin/bash
+
+echo Loesche data-Ordner
+echo -------------------
+sudo rm -r data
+echo
+
---
+++ db_start.sh
@@ -0,0 +1,9 @@
+#/bin/bash
+echo Baue Docker
+echo -----------
+sudo docker-compose build
+echo
+echo Starte Docker
+echo -------------
+sudo docker-compose up
+
