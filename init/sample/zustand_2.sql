8d4166ae962183e20d53816dab3de074c01a2356
ajtrs <jonathan.seifert@student.uni-halle.de>
2020-07-27 22:06:46 +0200

    SQL Skripte für Zustandsbefüllung begonnen

---
+++ init/sample/zustand_2.sql
@@ -0,0 +1,26 @@
+INSERT INTO land(land_id, land_name) values
+('DE', 'Deutschland'),
+('BE', 'Belgien'),
+('FR', 'Frankreich'),
+('AT', 'Österreich'),
+('CN', 'China'),
+('GB', 'Großbrittanien'),
+('US', 'Vereinigte Staaten von Amerika');
+
+INSERT INTO bundesland(bundesland_id, land_id, bundesland_name) values
+('DE:BW', 'DE', 'Baden-Württemberg'),
+('DE:BY', 'DE', 'Bayern'),
+('DE:BE', 'DE', 'Berlin'),
+('DE:BB', 'DE', 'Brandenburg'),
+('DE:HB', 'DE', 'Bremen'),
+('DE:HH', 'DE', 'Hamburg'),
+('DE:HE', 'DE', 'Hessen'),
+('DE:MV', 'DE', 'Mecklemburg-Vorpommern'),
+('DE:NI', 'DE', 'Niedersachsen'),
+('DE:NW', 'DE', 'Nordrhein-Westfahlen'),
+('DE:RP', 'DE', 'Reinland-Pfalz'),
+('DE:SL', 'DE', 'Saarland'),
+('DE:SN', 'DE', 'Sachsen'),
+('DE:ST', 'DE', 'Sachsen-Anhalt'),
+('DE:SH', 'DE', 'Schleswig-Holstein'),
+('DE:TH', 'DE', 'Thüringen');
